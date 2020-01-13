6528c645165441a25f217136e8d8ff2eca4a6ff2
jnstong <31449878+jnstong@users.noreply.github.com>
2020-01-12 17:42:31 -0700

    Add project files.

---
+++ WiredBrainCoffeeShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WiredBrainCoffeeShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        }
+    }
+}
